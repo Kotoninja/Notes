a1cb4085a64d37fe54cccc76c22dae44323bb00f
artem <SS4T46@yandex.ru>
2025-12-20 21:25:28 +0300

    Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        } />
+        {/* Auth */}
+        <Route path="/login/" element={<Login />} />
+
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="*" element={< NotFound />} />
       </Routes>
     </BrowserRouter>
