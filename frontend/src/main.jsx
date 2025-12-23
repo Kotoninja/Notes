4f54c4d43b0f416d30253e5a0fa6f6bd2ab2d83a
artem <SS4T46@yandex.ru>
2025-12-23 23:13:56 +0300

    snowfall

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Snowfall from 'react-snowfall'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <div style={{ position: 'relative' }}>
+      <App />
+      <div>
+        <Snowfall style={{
+          position: 'fixed',
+          width: '100vw',
+          height: '100vh',
+        }} snowflakeCount={150} />
+      </div>
+    </div>
+  </StrictMode>
 )
