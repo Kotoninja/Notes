235055801a47b5d073a406dcc3dd7741d838a5f1
artem <SS4T46@yandex.ru>
2025-12-24 22:11:01 +0300

    Fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ACCESS_TOKEN } from "./constants";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
@@ -17,5 +17,25 @@ api.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+
+api.interceptors.response.use(
+    undefined,
+    async (error) => {
+        const originalRequest = { ...error.config };
+        originalRequest._isRetry = true;
+
+        if (error.status === 401 && error.config && !error.config._isRetry) {
+            try {
+                const response = await api.post("api/token/refresh/", { refresh: localStorage.getItem(REFRESH_TOKEN) });
+                localStorage.setItem(ACCESS_TOKEN, response.data.access);
+                return api.request(originalRequest);
+            } catch (error) {
+                console.log(error);
+            };
+        };
+        
+        throw error;
+    }
+)
 
 export default api;
