e2944e98436400fbd09a4ee8b83ffab569310296
artem <SS4T46@yandex.ru>
2026-01-07 20:51:00 +0300

    fix cache invalidation

--- backend/note/models.py
+++ backend/note/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+from django.core.cache import cache
 
 
 class Note(models.Model):
@@ -9,3 +12,8 @@ class Note(models.Model):
     publication_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(blank=True, null=True)
     completed = models.BooleanField(default=False)
+
+
+@receiver(signal=[post_delete, post_save], sender=Note)
+def note_cache_invalidation(sender, instance, **kwargs):
+    cache.delete(key="user:{id}:notes:all".format(id=instance.user.id))
