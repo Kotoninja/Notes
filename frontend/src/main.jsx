6b647fe2b405feb398a61f5d8f6662536fe5c00a
artem <SS4T46@yandex.ru>
2026-01-08 19:28:31 +0300

    Delete snowfall

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import Snowfall from 'react-snowfall';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div style={{ position: 'relative' }}>
       <App />
-      <div>
-        <Snowfall style={{
-          position: 'fixed',
-          width: '100vw',
-          height: '100vh',
-        }} snowflakeCount={150}
-          color='rgba(25, 118, 210, 0.4)' />
-      </div>
-    </div>
   </StrictMode>
 );
