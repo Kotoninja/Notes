21eb04246b35995e5e81b5011ee2def447dddaa9
artem <SS4T46@yandex.ru>
2026-01-17 13:14:01 +0300

    NoteUpdateSerialier

--- backend/note/serializers.py
+++ backend/note/serializers.py
@@ -26,3 +26,17 @@ class NoteCreateSerializer(serializers.ModelSerializer):
             **{**validated_data, "completed": False},
         )
         return note
+
+
+class NoteUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["user", "title", "description", "completed", "publication_date"]
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.end_date = validated_data.get("end_date", instance.end_date)
+        instance.completed = validated_data.get("completed", instance.completed)
+        instance.save()
+        return instance
