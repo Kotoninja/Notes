b9b954ebc8a54afae438668a2d558b2139e6f6c6
artem <SS4T46@yandex.ru>
2026-01-24 20:32:28 +0300

    The project serializers have been moved into one file.

--- backend/project/views.py
+++ backend/project/views.py
@@ -9,12 +9,16 @@ from django.shortcuts import get_object_or_404
 from note.models import Note
 from note.serializers import NoteDetailOutputSerializer
 from typing import cast
+from project.serializers import (
+    ProjectDetailOutputSerializer,
+    ProjectInputSerializer,
+    ProjectOutputSerializer,
+)
 
 """
-TODO 
-- [x] API CRUD
-- [ ] Cache
-- [ ] Update OpenAPI
+[x] API CRUD
+[ ] Cache
+[ ] Update OpenAPI
 """
 
 
@@ -22,41 +26,8 @@ class ProjectAPI(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     permission_classes = [IsAuthenticated]
 
-    class ProjectOutputSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Project
-            fields = ["id", "name", "color"]
-
-    class ProjectDetailOutputSerializer(serializers.ModelSerializer):
-        notes = NoteDetailOutputSerializer(many=True, read_only=True)
-
-        class Meta:
-            model = Project
-            exclude = ["user"]
-
-    class ProjectInputSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Project
-            exclude = ["user"]
-            read_only_fields = ["id"]
-
-        def create(self, validated_data):
-            print("Validate data in create", validated_data)
-            new_project = Project.objects.create(**validated_data)
-            print(new_project)
-            return new_project
-
-        def update(self, instance, validated_data):
-            instance.name = validated_data.get("name", instance.name)
-            instance.description = validated_data.get(
-                "description", instance.description
-            )
-            instance.color = validated_data.get("color", instance.color)
-            instance.save()
-            return instance
-
     def list(self, request):
-        serializer = self.ProjectOutputSerializer(
+        serializer = ProjectOutputSerializer(
             self.queryset.filter(user=request.user), many=True
         )
         return Response(data=serializer.data, status=status.HTTP_200_OK)
@@ -65,11 +36,11 @@ class ProjectAPI(viewsets.ModelViewSet):
         project = get_object_or_404(
             self.queryset.prefetch_related("notes"), pk=pk, user=request.user
         )
-        serializer = self.ProjectDetailOutputSerializer(project)
+        serializer = ProjectDetailOutputSerializer(project)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
-        serializer = self.ProjectInputSerializer(data=request.data)
+        serializer = ProjectInputSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             new_project = cast(Project, serializer.save(user=request.user))
             response_data = dict(serializer.data) | {"id": new_project.pk}
@@ -78,9 +49,7 @@ class ProjectAPI(viewsets.ModelViewSet):
 
     def partial_update(self, request, pk):
         project = get_object_or_404(Project, pk=pk)
-        serializer = self.ProjectInputSerializer(
-            project, data=request.data, partial=True
-        )
+        serializer = ProjectInputSerializer(project, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
