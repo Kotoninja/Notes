78e3871e22f66d8177da86a84240ad6d9bc38557
artem <SS4T46@yandex.ru>
2026-01-24 19:49:20 +0300

    Project API

--- backend/project/models.py
+++ backend/project/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import random
 
 
@@ -12,13 +14,14 @@ class Project(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+        
         if not self.name:
             self.name = f"Project #{self.pk}"
 
         if not self.color:
             self.color = f"#{hex(random.randrange(2**24))[2:]}"
 
-        return super(Project, self).save(*args, **kwargs)
+        return super(Project, self).save(update_fields=["name", "color"])
 
     def __str__(self):
-        return f"{self.user.pk} - {self.name}"
+        return f"User - {self.user.pk} : {self.pk} - {self.name} {self.color and '- colored'}"
