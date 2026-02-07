e0d998360748161972d2a88c47591ef2faebabdf
artem <SS4T46@yandex.ru>
2026-02-07 19:02:41 +0300

    fix: ProjectListSettings

---
+++ frontend/src/components/projects/ProjectFieldListSettings.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Popper from '@mui/material/Popper';
+import Paper from '@mui/material/Paper'
+
+
+export function ProjectFieldListSettings(props) {
+    return (
+        <Popper open={true} anchorEl={props.anchorEl} id={`popper-${props.id}`}>
+            <Paper sx={{ width: "150px" }}>
+                <List component="div" sx={{ borderRadius: "4px", mt: "4px", border: 1, borderColor: 'grey.700', }} disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon sx={{ minWidth: "35px" }}>
+                            <DriveFileRenameOutlineIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"Rename"} />
+                    </ListItemButton>
+                    <ListItemButton sx={{color: "red"}}>
+                        <ListItemIcon sx={{ minWidth: "35px" }}>
+                            <DeleteIcon sx={{ color: "red" }} />
+                        </ListItemIcon>
+                        <ListItemText primary="Delete" />
+                    </ListItemButton>
+                </List>
+            </Paper>
+        </Popper>
+    )
+};
