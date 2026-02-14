0201e9a3c884d913fd951073f0fd373757b760e3
artem <SS4T46@yandex.ru>
2026-02-14 17:26:42 +0300

    fix: user urls

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "user"
 
 urlpatterns = [
-    path("users/", view=views.UserAPIList.as_view(), name="users"),
     path("registration/", view=views.UserApi.as_view({"post":"create"}), name="registration"),
     path("context/", view=views.UserApi.as_view({"get":"list"}), name="registration"),
 ]
