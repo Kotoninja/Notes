2d37c3918e442177f47352693a4fc63459666ac5
artem <SS4T46@yandex.ru>
2026-02-23 01:20:40 +0300

    fix: note urls

--- backend/note/urls.py
+++ backend/note/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
+
 from note import views
 
+app_name = "note"
+
 urlpatterns = [
-    path("all/", view=views.NoteApi.as_view({"get": "list"}), name="notes"),
-    path("create/", view=views.NoteApi.as_view({"post": "create"}), name="notes"),
-    path("update/<int:pk>/", view=views.NoteApi.as_view({"put": "update"}), name="notes"),
-    path("delete/<int:pk>/", view=views.NoteApi.as_view({"delete": "destroy"}), name="notes"),
+    path("all/", view=views.NoteApi.as_view({"get": "list"}), name="all"),
+    path("create/", view=views.NoteApi.as_view({"post": "create"}), name="create"),
+    path(
+        "update/<int:pk>/", view=views.NoteApi.as_view({"put": "update"}), name="update"
+    ),
+    path(
+        "delete/<int:pk>/",
+        view=views.NoteApi.as_view({"delete": "destroy"}),
+        name="delete",
+    ),
 ]
