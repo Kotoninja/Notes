c801a66b7e0f90a2e2a69d2bf90340b36207347d
artem <SS4T46@yandex.ru>
2026-03-09 14:06:00 +0300

    feat: date verification in note models

--- backend/note/models.py
+++ backend/note/models.py
@@ -1,9 +1,11 @@
 from cache.keys import PROJECT_DETAIL
 from django.contrib.auth.models import User
 from django.core.cache import cache
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+from django.utils import timezone
 from project.models import Project
 
 
@@ -17,6 +19,17 @@ class Note(models.Model):
     publication_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField(blank=True, null=True)
     completed = models.BooleanField(default=False)
+
+    def clean(self, *args, **kwargs):
+        if self.end_date is not None and self.end_date < timezone.now():
+            raise ValidationError(
+                "The end date cannot be less than the current date.",
+                params={"end_date": self.end_date},
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
 
 @receiver(signal=[post_delete, post_save], sender=Note)
